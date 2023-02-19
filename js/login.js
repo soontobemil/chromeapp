d755139c1f72f38ba8d08cf102c55ceb62951d3b
Jay Sim <jay.sim@vshn.net>
2023-02-18 17:43:39 -0800

    weather

--- js/login.js
+++ js/login.js
@@ -1,32 +1,28 @@
-// when a user submit an event, it will save in the localstorage
-// example: 고양이, username, myname.....
-// depending on the user input, it can show or hide the login form
-// greeting message with the user name
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector ("#login-form input");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit (event) {
+function onLoginSubmit(event){
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginForm.classList.add(HIDDEN_CLASSNAME)
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreeting(username);
 }
 
 function paintGreeting(username) {
-    greeting.innerText = `HELLO ${username} !`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Welcome to Cypress snowboarder: ${username} !`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 if(savedUserName === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreeting(savedUserName);
+  paintGreeting(savedUserName);
 }
--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,28 @@
-// using openweathermap API to get live weather and temperature
+const API_KEY = "6af213b58ec1dbbaa704dfc82d187d76";
+
+
+function onGeoOK(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url =
+     `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; // call API
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:nth-child(2)");
+        const temp = document.querySelector("#weather span:last-child");
+        const name = data.name;
+        weather.innerText = data.weather[0].main;
+        city.innerText = data.name;
+        temp.innerText = `${Math.floor(data.main.temp)}˚C`;})
+    // Javascript가 브라우저에 요청하여 API url에 방문하지 않아도 url내부의 자체정보를 브라우저로 가져옴 (Network 탭에서 확인)
+
+    }
+
+function onGeoError(){
+    alert("Can't find you!");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
